c21e7d983acf4cffa3461cbba8b40faec6022a50
Mahdi Parivash <mahdiparivsh@protonmail.com>
2022-03-22 17:31:06 +0430

    Project Complate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import Home from "./pages/Home";
+
 import { BrowserRouter as Router } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 function App() {
   return (
